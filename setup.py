cfbc98abe7cc130fdf76048fd09a042333fb381d
taikooo <52569157+taikoooo@users.noreply.github.com>
2024-04-15 04:30:28 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 setuptools.setup(
     name="format_print",
     version="1.0",
-    author="taikoooo",
+    author="taikooo",
     author_email="bbstufo@gmail.com",
     description="一个用于格式化输出的库，主要功能包括多行输出刷新、全半角字符混合对齐、秒数及字节大小格式化等",
     long_description=long_description,
@@ -13,7 +13,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache License",
         "Operating System :: OS Independent",
     ],
     install_requires=[],
